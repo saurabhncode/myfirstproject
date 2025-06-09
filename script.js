3eb53acdffbe104e5f615be8fba8041efb472749
saurabhncode <sanjunegi510@gmail.com>
2025-06-09 12:10:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function startExam() {
-  window.location.href = "questions.html";
-}
-function selectSubject(subject) {
-  alert("You selected: " + subject);
-   localStorage.setItem("selectedSubject", subject);
-}
+
